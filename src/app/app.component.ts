86c8ce8db0355d0f776f15cb971094bd4e0e702d
Krzysztof Dragon <k.dragon@wasko.pl>
2022-07-07 11:58:51 +0200

    Zrobione az do wykładu nr. 90

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { STATE } from './success/success.component'
+import { Navigation } from './shared/navigation.model';
+import { STATE } from './success/success.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
+  Navigation = Navigation;
+  loadedRoute = Navigation.RECIPES;
+
+  onNavigate(route: Navigation) {
+    this.loadedRoute = route;
+  }
 }
